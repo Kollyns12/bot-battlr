5631bd1f507dd0e823f5e6f2e492eaabf81898bf
Kollyns12 <collinsmoses295@gmail.com>
2025-10-27 13:15:26 +0300

    my updated db.json file

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bot-battlr/', 
   plugins: [react()],
   server: {
     port: 3000,
